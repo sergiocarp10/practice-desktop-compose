8356a50e5e883b6aa381f920827a39d00e4e416f
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2023-01-08 18:44:40 -0300

    practicando con textos editables

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Cambios respecto a Android
+
+La función donde se lanza la aplicación es ``main()`` en lugar de ``onCreate()``.
+
+Se puede especificar el título de la ventana como parámetro del objeto ``Window``
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,32 +1,84 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+
+    var name by remember { mutableStateOf("") }
+    var score by remember { mutableStateOf("") }
+    var scoreNumber by remember { mutableStateOf<Int?>(null) }
+    val enabled = scoreNumber != null && name.isNotBlank()
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = {name = it},
+                label = { Text("Nombre") }
+            )
+
+            OutlinedTextField(
+                value = score,
+                onValueChange = {
+                    val result = updateScoreInput(it)
+                    score = result.first?: score
+                    scoreNumber = result.second
+                },
+                label = { Text("Puntaje") }
+            )
+
+            Button(
+                onClick = { },
+                enabled = enabled
+            ) {
+                Text("Guardar")
+            }
         }
+
+
     }
 }
 
+private fun updateScoreInput(input: String) : Pair<String?, Int?> {
+    if (input.isBlank() || input == "-"){
+        return Pair(input, null)
+    }
+
+    val aux = input.toIntOrNull()
+    if (aux != null) return Pair(input, aux)
+
+    return Pair(null, null)
+}
+
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Desktop Compose Practice"
+    ) {
         App()
     }
 }
